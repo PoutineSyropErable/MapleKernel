467a3805f8fea1adc7a3a4c39525f3c578e59a39
François Pagé <francois.page@mail.mcgill.ca>
2025-03-25 00:30:12 -0400

    changed build.sh to use a build dir

--- Tutorials/BareBones/build.sh
+++ Tutorials/BareBones/build.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-i686-elf-as boot.s -o boot.o
-i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+# Define directories
+BUILD_DIR="build"
+ISO_DIR="isodir"
 
-if grub-file --is-x86-multiboot myos.bin; then
-	echo multiboot confirmed
+# Create necessary directories
+mkdir -p "$BUILD_DIR" "$ISO_DIR/boot/grub"
+
+# Assemble the bootloader assembly
+i686-elf-as boot.s -o "$BUILD_DIR/boot.o"
+
+# Compile the kernel
+i686-elf-gcc -c kernel.c -o "$BUILD_DIR/kernel.o" -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+
+# Link the kernel and generate the final binary
+i686-elf-gcc -T linker.ld -o "$BUILD_DIR/myos.bin" -ffreestanding -O2 -nostdlib "$BUILD_DIR/boot.o" "$BUILD_DIR/kernel.o" -lgcc
+
+# Check if the kernel is multiboot-compliant
+if grub-file --is-x86-multiboot "$BUILD_DIR/myos.bin"; then
+	echo "Multiboot confirmed"
 else
-	echo the file is not multiboot
+	echo "The file is not multiboot"
 	exit 1
 fi
 
-mkdir -p isodir/boot/grub
-cp myos.bin isodir/boot/myos.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
-grub-mkrescue -o myos.iso isodir
+# Copy the kernel binary and GRUB configuration to the ISO directory
+cp "$BUILD_DIR/myos.bin" "$ISO_DIR/boot/myos.bin"
+cp grub.cfg "$ISO_DIR/boot/grub/grub.cfg"
+
+# Create the ISO image
+grub-mkrescue -o "$BUILD_DIR/myos.iso" "$ISO_DIR"
+
+echo "ISO created successfully: $BUILD_DIR/myos.iso"
