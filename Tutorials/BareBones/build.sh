e9c9b4403a29a4b2cb5aaa13d0f3a264854a8fbc
François Pagé <francois.page@mail.mcgill.ca>
2025-03-25 00:28:47 -0400

    nice

--- Tutorials/BareBones/build.sh
+++ Tutorials/BareBones/build.sh
@@ -8,4 +8,10 @@ if grub-file --is-x86-multiboot myos.bin; then
 	echo multiboot confirmed
 else
 	echo the file is not multiboot
+	exit 1
 fi
+
+mkdir -p isodir/boot/grub
+cp myos.bin isodir/boot/myos.bin
+cp grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o myos.iso isodir
