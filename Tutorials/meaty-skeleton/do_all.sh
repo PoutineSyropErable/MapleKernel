28f079f194ce776f9380dc494157b30154923e3a
François Pagé <francois.page@mail.mcgill.ca>
2025-03-25 18:50:42 -0400

    better do all

--- Tutorials/meaty-skeleton/do_all.sh
+++ Tutorials/meaty-skeleton/do_all.sh
@@ -1,16 +1,22 @@
-#!/bin/bash 
+#!/bin/bash
+set -e
 
+printf -- "\n\n\n===========Cleaning============\n\n\n"
 ./clean.sh
+printf -- "\n\n\n===========Headers============\n\n\n"
 ./headers.sh
+printf -- "\n\n\n===========Iso============\n\n\n"
 ./iso.sh
 
-./qemu.sh & 
+printf -- "\n\n\n===========qemu============\n\n\n"
+./qemu.sh &
 # qemu-system-x86_64 -drive format=raw,file=hello.bin &
 QEMU_PID=$!
 
 # Give QEMU a second to start up
 sleep 1
 
+printf -- "\n\n\n===========VNC seeing============\n\n\n"
 # Launch VNC viewer
 vncviewer localhost:5900
 
