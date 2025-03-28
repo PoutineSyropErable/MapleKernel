a06e0cc144d344cbaf917bd18a1c91f9fae46b2a
François Pagé <francois.page@mail.mcgill.ca>
2025-03-28 16:58:58 -0400

    generate compile command.json

---
+++ Tutorials/meaty-skeleton/aaa_do_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+printf -- "\n\n\n===========Cleaning============\n\n\n"
+./clean.sh
+printf -- "\n\n\n===========Headers============\n\n\n"
+./headers.sh
+
+# skip QEMU, and iso when building for bear
+if [[ "$1" != "--bear" ]]; then
+	printf -- "\n\n\n===========Iso============\n\n\n"
+	./iso.sh
+
+	printf -- "\n\n\n===========qemu============\n\n\n"
+	./qemu.sh
+	# qemu-system-x86_64 -drive format=raw,file=hello.bin &
+fi
+
+# vnc is in qemu.sh
