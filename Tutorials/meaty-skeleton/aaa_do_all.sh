cfb25b7787eeecd23406b21178973bafa9bb664b
François Pagé <francois.page@mail.mcgill.ca>
2025-03-28 17:08:48 -0400

    updates

--- Tutorials/meaty-skeleton/aaa_do_all.sh
+++ Tutorials/meaty-skeleton/aaa_do_all.sh
@@ -6,10 +6,11 @@ printf -- "\n\n\n===========Cleaning============\n\n\n"
 printf -- "\n\n\n===========Headers============\n\n\n"
 ./headers.sh
 
+printf -- "\n\n\n===========Iso============\n\n\n"
+./iso.sh
+
 # skip QEMU, and iso when building for bear
 if [[ "$1" != "--bear" ]]; then
-	printf -- "\n\n\n===========Iso============\n\n\n"
-	./iso.sh
 
 	printf -- "\n\n\n===========qemu============\n\n\n"
 	./qemu.sh
