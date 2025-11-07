7317c95bc989f2dcece6f94494814f7e9e06acd6
François Pagé <francois.page@mail.mcgill.ca>
2025-11-07 15:52:56 -0500

    now it pushes argc first, good

--- Tutorials/Variable_Args/push_var_args.c
+++ Tutorials/Variable_Args/push_var_args.c
@@ -5,14 +5,16 @@
 
 uint16_t args16_start[32];
 
-void push_to_args16(uint16_t num, ...) {
+void push_to_args16(uint16_t argc, ...) {
 
 	va_list args;
-	va_start(args, num);
+	va_start(args, argc);
 
-	for (int i = 0; i < num; i++) {
+	args16_start[0] = argc;
+
+	for (int i = 0; i < argc; i++) {
 		uint16_t value = va_arg(args, int);
-		args16_start[i] = value;
+		args16_start[i + 1] = value;
 	}
 }
 
@@ -24,7 +26,7 @@ int main(void) {
 
 	push_to_args16(argc, 12, 56, 259, 4269);
 
-	for (uint16_t i = 0; i < argc; i++) {
+	for (uint16_t i = 0; i < argc + 1; i++) {
 		printf("%u : %u\n", i, args16_start[i]);
 	}
 
