71c5a7b21a96d4a85a44665ef9c9abc650ddffb8
François Pagé <francois.page@mail.mcgill.ca>
2025-11-07 15:49:50 -0500

    ok, got a starter version

--- Tutorials/Variable_Args/push_var_args.c
+++ Tutorials/Variable_Args/push_var_args.c
@@ -1,20 +1,32 @@
 #include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 
-void print_ints(int num, ...) {
+uint16_t args16_start[32];
+
+void push_to_args16(uint16_t num, ...) {
 
 	va_list args;
 	va_start(args, num);
 
 	for (int i = 0; i < num; i++) {
-		int value = va_arg(args, int);
-		printf("%d: %d\n", i, value);
+		uint16_t value = va_arg(args, int);
+		args16_start[i] = value;
 	}
 }
 
 int main(void) {
 
-	printf("\n====Start of program======\n");
+	printf("\n====Start of program======\n\n");
+
+	uint16_t argc = 4;
+
+	push_to_args16(argc, 12, 56, 259, 4269);
+
+	for (uint16_t i = 0; i < argc; i++) {
+		printf("%u : %u\n", i, args16_start[i]);
+	}
 
 	return 0;
 }
