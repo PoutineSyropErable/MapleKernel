0dd7e8f3e95f07ad4d5bbc547b0208a46f24cf3b
François Pagé <francois.page@mail.mcgill.ca>
2025-11-07 22:25:39 -0500

    text segment name change for realmode code

--- Tutorials/BareBones/realmode_functions.c
+++ Tutorials/BareBones/realmode_functions.c
@@ -1,8 +1,8 @@
 #include "realmode_functions.h"
 #include <stdint.h>
 
-/* Place this function into the custom section ".text_realmode_functions" */
-__attribute__((naked, section(".text_realmode_functions"))) uint16_t add16(uint16_t a, uint16_t b) {
+/* Place this function into the custom section ".text.realmode_functions" */
+__attribute__((naked, section(".text.realmode_functions"))) uint16_t add16(uint16_t a, uint16_t b) {
 	__asm__(
 	    ".code16\n"
 	    ".intel_syntax noprefix\n"
@@ -20,13 +20,13 @@ __attribute__((naked, section(".text_realmode_functions"))) uint16_t add16(uint1
 }
 
 /* Reference version (purely for comparison) */
-__attribute__((section(".text_realmode_functions"))) uint16_t add16_ref(uint16_t a, uint16_t b) {
+__attribute__((section(".text.realmode_functions"))) uint16_t add16_ref(uint16_t a, uint16_t b) {
 
 	return 2 * a + b;
 }
 
 /* Reference version (purely for comparison) */
-__attribute__((section(".text_realmode_functions"))) int16_t complex_operation(uint16_t a, uint16_t b, uint16_t c, uint16_t d) {
+__attribute__((section(".text.realmode_functions"))) int16_t complex_operation(uint16_t a, uint16_t b, uint16_t c, uint16_t d) {
 
 	return 2 * a + b - c + 3 * d;
 }
--- Tutorials/BareBones/realmode_functions.h
+++ Tutorials/BareBones/realmode_functions.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
-__attribute__((naked, section(".text_realmode_functions"))) uint16_t add16(uint16_t a, uint16_t b);
+__attribute__((naked, section(".text.realmode_functions"))) uint16_t add16(uint16_t a, uint16_t b);
 
-__attribute__((section(".text_realmode_functions"))) uint16_t add16_ref(uint16_t a, uint16_t b);
+__attribute__((section(".text.realmode_functions"))) uint16_t add16_ref(uint16_t a, uint16_t b);
 
-__attribute__((section(".text_realmode_functions"))) int16_t complex_operation(uint16_t a, uint16_t b, uint16_t c, uint16_t d);
+__attribute__((section(".text.realmode_functions"))) int16_t complex_operation(uint16_t a, uint16_t b, uint16_t c, uint16_t d);
