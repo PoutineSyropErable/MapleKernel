e860c6160f3bf225b9a8ded074625474606daf9e
François Pagé <francois.page@mail.mcgill.ca>
2025-12-29 19:29:38 -0500

    ok, now its properly working with the new locations

---
+++ src/ProtectedMode/z_otherLang/bkup/zig/kernel_zig.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int zig_main(void); // C calls this
+
+#ifdef __cplusplus
+}
+#endif
--- src/ProtectedMode/z_otherLang/zig/build.sh
+++ src/ProtectedMode/z_otherLang/zig/build.sh
@@ -6,7 +6,24 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Building in: $SCRIPT_DIR"
 
 cd "$SCRIPT_DIR"
+
+function find_git_root() {
+	local dir=${1:-$PWD} # start from given dir or current directory
+	while [[ "$dir" != "/" ]]; do
+		if [[ -d "$dir/.git" ]]; then
+			echo "$dir"
+			return 0
+		fi
+		dir=$(dirname "$dir")
+	done
+	echo "No git repository found" >&2
+	return 1
+}
+
+project_root=$(find_git_root)
+
+BUILD_DIR="$project_root/build32"
 zig build "$@"
 
 # mv ./zig-out/lib/libkernel_zig.a ../../build/libkernel_zig.a
-mv ./zig-out/kernel_zig.o ../../build/kernel_zig.o
+mv ./zig-out/kernel_zig.o "$BUILD_DIR/kernel_zig.o"
